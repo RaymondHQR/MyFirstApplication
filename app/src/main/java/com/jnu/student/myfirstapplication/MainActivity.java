42d4ea24a7b8d4ad75a95fa3fb7dd6739054af63
LiangChengHQR <1439056345@qq.com>
2023-10-07 17:28:45 +0800

    [feat] 实验三

--- app/src/main/java/com/jnu/student/myfirstapplication/MainActivity.java
+++ app/src/main/java/com/jnu/student/myfirstapplication/MainActivity.java
@@ -3,12 +3,18 @@ package com.jnu.student.myfirstapplication;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+
+    private TextView textView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        textView = findViewById(R.id.text_view_hello_world);
+        textView.setText(R.string.hello_android);
     }
 }
